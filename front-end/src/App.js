b66b43636a78edadbbe1bd0daba809b3f3a3f6c0
Souza_Luis <souza_lipes@outlook.com>
2024-05-14 20:53:14 -0300

    Finalização aplicação

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,40 +1,96 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import AWS from 'aws-sdk';
+import * as StepFunctions from "aws-sdk/clients/stepfunctions";
+
+AWS.config.update({
+  region: 'sa-east-1',
+});
 
 const MyComponent = () => {
   const [nota1, setNota1] = useState('');
   const [nota2, setNota2] = useState('');
   const [media, setMedia] = useState(null);
-  const [mesnsagemBoasVindas, setMensagemBoasVindas] = useState('');
+  const [mensagemBoasVindas, setMensagemBoasVindas] = useState('');
 
   const calcularMedia = async () => {
-    try {
-      const response = await axios.post('https://cx4ihg25343nqyhahd2ndrilra0gccto.lambda-url.sa-east-1.on.aws/', {
+ 
+      const response = await axios.post('https://al3ijok58l.execute-api.sa-east-1.amazonaws.com/dev/calcularMedia', {
         nota1: parseFloat(nota1),
         nota2: parseFloat(nota2)
       });
 
-      setMedia(response.data.message);
-    } catch (error) {
-      console.error('Ocorreu um erro ao calcular a média:', error);
-    }
+      setMedia(response.data.body);
+      console.log(response.data.body);
+
+      const stepfunctions = new StepFunctions({
+        region: 'sa-east-1', // Insira sua região da AWS aqui
+        credentials: {
+            accessKeyId: '',
+            secretAccessKey: ''
+        }
+      });
+      const params = {
+        stateMachineArn: 'arn:aws:states:sa-east-1:577163328740:stateMachine:MyStateMachine-ia8gmm1pm', 
+        input: JSON.stringify({         
+          nota1: parseFloat(nota1),
+          nota2: parseFloat(nota2) 
+        }), 
+      };
+
+      stepfunctions.startExecution(params, (err, data) => {
+        console.log('teste')
+        if (err) {
+          console.error('Erro ao iniciar a execução da máquina de estado:', err);
+        } else {
+          console.log('Máquina de estado iniciada com sucesso:', data);
+          
+        }
+      });
+
   };
 
   const boasVindas = async () => {
-    try {
       const response = await axios.get('https://al3ijok58l.execute-api.sa-east-1.amazonaws.com/dev/hello');
 
-      setMensagemBoasVindas(response.message);
-    } catch (error) {
-      console.error('Ocorreu um erro ao buscar a mensagem de boas vindas:', error);
+      setMensagemBoasVindas(response.data.body);
+
+      const stepfunctions = new StepFunctions({
+        region: 'sa-east-1', // Insira sua região da AWS aqui
+        credentials: {
+            accessKeyId: '',
+            secretAccessKey: ''
+        }
+      });
+      const params = {
+        stateMachineArn: 'arn:aws:states:sa-east-1:577163328740:stateMachine:MyHellow', 
+        input: JSON.stringify({}), 
+      };
+
+      stepfunctions.startExecution(params, (err, data) => {
+        if (err) {
+          console.error('Erro ao iniciar a execução da máquina de estado:', err);
+        } else {
+          console.log('Máquina de estado iniciada com sucesso:', data);
+          // Aqui você pode tratar a resposta, se necessário
+        }
+      });
+
+
+  };
+
+
+  useEffect(() => {
+    async function fetchData() {
+      await boasVindas();
+
     }
-  }
-
-
-  useEffect(() => boasVindas(), [mesnsagemBoasVindas]);
+    fetchData();
+  }, [mensagemBoasVindas]);
 
   return (
     <div>
+      <h1>{mensagemBoasVindas}</h1>
       <input type="text" value={nota1} onChange={(e) => setNota1(e.target.value)} placeholder="Nota 1" />
       <input type="text" value={nota2} onChange={(e) => setNota2(e.target.value)} placeholder="Nota 2" />
       <button onClick={calcularMedia}>Calcular Média</button>
